3e5cad55f6e759388310e2262cb9fbc3070f5d87
GumFruit <gg4r9hv3p5zg@protonmail.com>
2022-02-19 08:57:38 +0800

    Update ports and peers

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -43,7 +43,7 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN) {
-        return std::make_unique<CBaseChainParams>("", 19705, 19707);
+        return std::make_unique<CBaseChainParams>("", 19707, 19709);
     } else if (chain == CBaseChainParams::TESTNET) {
         return std::make_unique<CBaseChainParams>("testnet3", 29707, 29709);
     } else if (chain == CBaseChainParams::SIGNET) {
